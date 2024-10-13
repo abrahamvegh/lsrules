bb06688df4e1879ae5e92eec9f5dc911c09d3817
Abraham Vegh <me@abrahamvegh.com>
2024-10-13 14:06:59 -0400

    Make script output deterministic
    - IPv4 considered legacy, back of the bus
    - natsort for the lists

--- scripts/derp.php
+++ scripts/derp.php
@@ -3,15 +3,22 @@ $derpmap = 'https://login.tailscale.com/derpmap/default';
 $file = file_get_contents($derpmap);
 $json = json_decode($file);
 $ips = [];
+// IPv4 considered legacy
+$ips_legacy = [];
 
 foreach ($json->Regions as $region)
 {
 	foreach ($region->Nodes as $node)
 	{
-		$ips[] = $node->IPv4;
 		$ips[] = $node->IPv6;
+		$ips_legacy[] = $node->IPv4;
 	}
 }
 
-echo implode(', ', $ips);
+natsort($ips);
+natsort($ips_legacy);
+
+$merged = array_merge($ips, $ips_legacy);
+
+echo implode(', ', $merged);
 echo "\n";
